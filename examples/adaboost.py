d198169d9defcc306f39cd35983ce4a78b024e9b
Hiroyuki Tanaka <pillyshi21@gmail.com>
2018-08-25 06:02:49 +0900

    Add an AdaBoost example

--- examples/adaboost.py
+++ examples/adaboost.py
@@ -2,11 +2,14 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris, make_moons, make_circles
-from nonlinear_classifier import NonlinearClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+
+from simple_classifier import SimpleRandomBinaryClassifier
 
 
 def plot(X, y, clf):
@@ -29,7 +32,7 @@ def plot(X, y, clf):
 
 def load(dataset, random_state, n_samples):
     datasets = {
-        'iris': lambda: load_iris(return_X_y=True),
+        # 'iris': lambda: load_iris(return_X_y=True),
         'moons': lambda: make_moons(noise=0.05, random_state=random_state, n_samples=n_samples),
         'circles': lambda: make_circles(noise=0.05, factor=0.5, random_state=random_state, n_samples=n_samples)
     }
@@ -40,34 +43,25 @@ def load(dataset, random_state, n_samples):
 
 def main(args):
     X, y = load(args.dataset, args.random_state, args.n_samples)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=args.random_state)
 
-    n_classes = len(np.unique(y))
-    if n_classes > 2:
-        convert_y = False
-    else:
-        convert_y = True
-
-    clf = OneVsRestClassifier(NonlinearClassifier(
-        n_components=X.shape[0],
-        random_state=args.random_state,
-        convert_y=convert_y
-    ))
-    clf = GridSearchCV(clf, {'estimator__sigma': np.logspace(-2, 1, 10)}).fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print('best params:', clf.best_params_)
-    print('accuracy:', accuracy)
+    classifier = AdaBoostClassifier(SimpleRandomBinaryClassifier(
+        convert_y=True,
+    ), algorithm='SAMME', n_estimators=args.n_estimators, random_state=args.random_state)
+    pipe = Pipeline([
+        ('transformer', RBFSampler()),
+        ('classifier', classifier)
+    ])
+    print('accuracy:', cross_val_score(pipe, X, y, scoring='accuracy').mean())
 
     if args.plot:
         if args.dataset == 'iris':
             return
-        plot(X, y, clf)
+        plot(X, y, pipe)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='iris')
+    parser.add_argument('--dataset', type=str, default='moons')
     parser.add_argument('--n-samples', type=int, default=500)
     parser.add_argument('--random-state', type=int, default=None)
     parser.add_argument('--n-estimators', type=int, default=1000)
