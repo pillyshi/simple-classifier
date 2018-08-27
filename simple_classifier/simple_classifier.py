d9d7195cc73cb4457844ae09f56555eddbb27ef7
Hiroyuki Tanaka <pillyshi21@gmail.com>
2018-08-28 04:43:49 +0900

    Follow the sklearn convention

--- simple_classifier/simple_classifier.py
+++ simple_classifier/simple_classifier.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.multiclass import _ovr_decision_function
 from .utils import _check_y, _convert_y
 
 
@@ -19,14 +20,14 @@ class SimpleBinaryClassifier(BaseEstimator, ClassifierMixin):
             self.threshold = X.mean(axis=0)
         Z = 2 * (X >= self.threshold) - 1
         Z = Z * np.c_[sample_weight]
-        self.w = Z.T.dot(y) / n
+        self.w_ = Z.T.dot(y) / n
 
     def predict(self, X):
         return np.int32(self.decision_function(X) >= 0)
 
     def decision_function(self, X):
         Z = 2 * (X >= self.threshold) - 1
-        return Z.dot(self.w)
+        return Z.dot(self.w_)
 
 
 class SimpleRandomBinaryClassifier(BaseEstimator, ClassifierMixin):
@@ -44,18 +45,18 @@ class SimpleRandomBinaryClassifier(BaseEstimator, ClassifierMixin):
         n, d = X.shape
         rnd = np.random.RandomState(self.random_state)
         mean = X.mean(axis=0)
-        self.threshold = rnd.multivariate_normal(mean, np.identity(d) * self.sigma)
+        self.threshold_ = rnd.multivariate_normal(mean, np.identity(d) * self.sigma)
         Z = 2 * (X >= self.threshold) - 1
         Z = Z * np.c_[sample_weight]
-        self.w = Z.T.dot(y) / n
+        self.w_ = Z.T.dot(y) / n
         return self
 
     def predict(self, X):
         return np.int32(self.decision_function(X) >= 0)
 
     def decision_function(self, X):
-        Z = 2 * (X >= self.threshold) - 1
-        return Z.dot(self.w)
+        Z = 2 * (X >= self.threshold_) - 1
+        return Z.dot(self.w_)
 
 # TODO: implement SimpleClassifier which can handle multiclass classification
 # TODO: implement SimpleRandomClassifier which can handle multiclass classification
