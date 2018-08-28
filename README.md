2e3051c78b9d88d616351b8141b28ddc1d4f3c46
Hiroyuki Tanaka <pillyshi21@gmail.com>
2018-08-29 03:35:21 +0900

    Add benchmark

--- README.md
+++ README.md
@@ -11,3 +11,81 @@ pipenv run python -m examples.example --dataset iris
 pipenv run python -m examples.example --dataset moons --plot
 pipenv run python -m examples.example --dataset circles --plot
 ```
+
+## Benchmark
+
+The following command
+
+```bash
+pipenv run python -W ignore -m examples.benchmark --random-state 0
+```
+
+outputs
+
+```
+The performance compariton:
+--------------------
+dataset=moons, n_samples=100, n_features=2
+Logistic Regression: 0.85
+Logistic Regression (nonlinear): 0.94
+AdaBoost Classifier: 0.92
+AdaBoost Classifier (nonlinear): 0.93
+Simple Classifier: 0.8
+Simple Classifier (nonlinear): 0.92
+AdaBoost Simple Classifier: 0.91
+AdaBoost Simple Classifier (nonlinear): 0.95
+====================
+dataset=circles, n_samples=100, n_features=2
+Logistic Regression: 0.45
+Logistic Regression (nonlinear): 0.9
+AdaBoost Classifier: 0.9
+AdaBoost Classifier (nonlinear): 0.88
+Simple Classifier: 0.4
+Simple Classifier (nonlinear): 0.88
+AdaBoost Simple Classifier: 0.76
+AdaBoost Simple Classifier (nonlinear): 0.91
+====================
+dataset=linearly separable, n_samples=100, n_features=2
+Logistic Regression: 0.95
+Logistic Regression (nonlinear): 0.96
+AdaBoost Classifier: 0.89
+AdaBoost Classifier (nonlinear): 0.97
+Simple Classifier: 0.95
+Simple Classifier (nonlinear): 0.95
+AdaBoost Simple Classifier: 0.97
+AdaBoost Simple Classifier (nonlinear): 0.97
+====================
+
+The computational time comparison:
+--------------------
+dataset=moons, n_samples=100, n_features=2
+Logistic Regression: 0.00041961669921875 seconds
+Logistic Regression (nonlinear): 0.0010848045349121094 seconds
+AdaBoost Classifier: 0.06579995155334473 seconds
+AdaBoost Classifier (nonlinear): 0.09208011627197266 seconds
+Simple Classifier: 0.000202178955078125 seconds
+Simple Classifier (nonlinear): 0.0007181167602539062 seconds
+AdaBoost Simple Classifier: 0.030386924743652344 seconds
+AdaBoost Simple Classifier (nonlinear): 0.08698391914367676 seconds
+====================
+dataset=circles, n_samples=100, n_features=2
+Logistic Regression: 0.00030803680419921875 seconds
+Logistic Regression (nonlinear): 0.001074075698852539 seconds
+AdaBoost Classifier: 0.0614471435546875 seconds
+AdaBoost Classifier (nonlinear): 0.09307098388671875 seconds
+Simple Classifier: 0.00019097328186035156 seconds
+Simple Classifier (nonlinear): 0.0006070137023925781 seconds
+AdaBoost Simple Classifier: 0.030407190322875977 seconds
+AdaBoost Simple Classifier (nonlinear): 0.08505487442016602 seconds
+====================
+dataset=linearly separable, n_samples=100, n_features=2
+Logistic Regression: 0.0004889965057373047 seconds
+Logistic Regression (nonlinear): 0.0014030933380126953 seconds
+AdaBoost Classifier: 0.05795001983642578 seconds
+AdaBoost Classifier (nonlinear): 0.09112811088562012 seconds
+Simple Classifier: 0.0001957416534423828 seconds
+Simple Classifier (nonlinear): 0.0005719661712646484 seconds
+AdaBoost Simple Classifier: 0.03024888038635254 seconds
+AdaBoost Simple Classifier (nonlinear): 0.08540225028991699 seconds
+====================
+```
---
+++ examples/benchmark.py
@@ -0,0 +1,107 @@
+# Referrence:
+# http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+
+import time
+import argparse
+import warnings
+import numpy as np
+from sklearn.datasets import (
+    make_classification,
+    make_circles, make_moons
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+from simple_classifier import SimpleClassifier, SimpleRandomClassifier
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--random-state', type=int, default=None)
+    return parser.parse_args()
+
+
+def get_datasets(random_state):
+    X, y = make_classification(
+        n_features=2, n_redundant=0, n_informative=2,
+        random_state=1, n_clusters_per_class=1)
+    rnd = np.random.RandomState(random_state)
+    X += 2 * rnd.uniform(size=X.shape)
+    linearly_separable = (X, y)
+    return [
+        ('moons', make_moons(noise=0.3, random_state=random_state)),
+        ('circles', make_circles(noise=0.2, factor=0.5, random_state=random_state)),
+        ('linearly separable', linearly_separable)
+    ]
+
+
+def nonlinearize(classifier, random_state):
+    return Pipeline([
+        ('transformer', RBFSampler(random_state=random_state)),
+        ('classifier', classifier)
+    ])
+
+
+def get_classifiers(random_state):
+    names = [
+        'Logistic Regression',
+        'Logistic Regression (nonlinear)',
+        'AdaBoost Classifier',
+        'AdaBoost Classifier (nonlinear)',
+        'Simple Classifier',
+        'Simple Classifier (nonlinear)',
+        'AdaBoost Simple Classifier',
+        'AdaBoost Simple Classifier (nonlinear)',
+    ]
+    classifiers = [
+        LogisticRegression(random_state=random_state),
+        nonlinearize(LogisticRegression(random_state=random_state), random_state),
+        AdaBoostClassifier(random_state=random_state),
+        nonlinearize(AdaBoostClassifier(random_state=random_state), random_state),
+        SimpleClassifier(),
+        nonlinearize(SimpleClassifier(), random_state),
+        AdaBoostClassifier(SimpleRandomClassifier(random_state=random_state), algorithm='SAMME', random_state=random_state),
+        nonlinearize(AdaBoostClassifier(SimpleRandomClassifier(random_state=random_state), algorithm='SAMME', random_state=random_state), random_state)
+    ]
+    params = [
+        {'C': np.logspace(-4, 1, 20)},
+        {'transformer__gamma': np.logspace(-4, 1, 20), 'classifier__C': np.logspace(-4, 1, 20)},
+        {},
+        {'transformer__gamma': np.logspace(-4, 1, 20)},
+        {},
+        {'transformer__gamma': np.logspace(-4, 1, 20)},
+        {'base_estimator__sigma': np.logspace(-4, 0, 20)},
+        {'transformer__gamma': np.logspace(-4, 1, 20), 'classifier__base_estimator__sigma': np.logspace(-4, 0, 20)},
+    ]
+    return zip(names, classifiers, params)
+
+
+def main(args):
+    print('The performance compariton:')
+    print('-' * 20)
+    for dataset_name, (X, y) in get_datasets(args.random_state):
+        print('dataset={}, n_samples={}, n_features={}'.format(dataset_name, X.shape[0], X.shape[1]))    
+        cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=args.random_state)
+        for name, classifier, params in get_classifiers(args.random_state):
+            gs = GridSearchCV(classifier, params, scoring='accuracy', cv=cv, error_score=0).fit(X, y)
+            print('{}: {}'.format(name, gs.best_score_))
+        print('=' * 20)
+    print()
+    print('The computational time comparison:')
+    print('-' * 20)
+    for dataset_name, (X, y) in get_datasets(args.random_state):
+        print('dataset={}, n_samples={}, n_features={}'.format(dataset_name, X.shape[0], X.shape[1]))
+        for name, classifier, params in get_classifiers(args.random_state):
+            start = time.time()
+            classifier.fit(X, y)
+            print('{}: {} seconds'.format(name, time.time() - start))
+        print('=' * 20)
+
+
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
